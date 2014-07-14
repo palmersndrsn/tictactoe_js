a4ed852d30b014308d84c75c7d97f9249d1d9443
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-13 23:15:40 -0700

    Pretty much done but no winner

--- tictactoe.js
+++ tictactoe.js
@@ -11,28 +11,50 @@ var seven = document.getElementById("7")
 var eight = document.getElementById("8")
 var nine = document.getElementById("9")
 
-var count = 1
-var ex = []
-var zero = []
+var playerX = document.getElementById("player1")
+var playerO = document.getElementById("player2")
 
+var resetButton = document.getElementById("button")
+var allBoxes = document.getElementsByClassName("box")
+
+var count = 1
+var blank = 0
+
+//Reset (Tried for loop, while loop, and some other stuff. Why does only the for in loop work?)
+var reset = function (y) {
+	for (blank in allBoxes) {
+		y[blank].innerHTML = "";
+		playerX.innerHTML = "X";
+ 		playerO.innerHTML = "";
+		blank++;
+		count = 1;
+		}
+}
 
 // clicking
 var clicked = function (x) {
 if (x.innerHTML === "") {
 	if (count % 2 === 0) {
-		count ++
-		x.innerHTML = "X"
- 		x.style.color = "red"
- 		return ex 
-	}
-	else {
-		count ++
- 		x.innerHTML = "0"
- 		x.style.color = "blue"
- 		return zero
+		count ++;
+ 		playerX.innerHTML = "X";
+ 		playerO.innerHTML = "";
+		x.innerHTML = "O";
+ 		x.style.color = "blue";
 		}
-	
-	}
+	else {
+		count ++;
+ 		playerO.innerHTML = "O";
+ 		playerX.innerHTML = "";
+ 		x.innerHTML = "X";
+ 		x.style.color = "red";
+		};
+	};
+};
+//Possible switch for winner function (derp)
+
+//Reset Button
+resetButton.onclick = function () {
+	reset(allBoxes);
 }
 //Box Functions
 one.onclick = function () {
@@ -62,8 +84,6 @@ eight.onclick = function () {
 nine.onclick = function () {
 	clicked(nine);
 }
-console.log(zero)
-console.log(ex)
 
 }
 
